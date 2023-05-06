ed218393e4d1f6c1ebcd07116d3cfe31da2aee44
kabir1412 <87866985+kabir1412@users.noreply.github.com>
2023-05-06 18:40:01 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,9 @@
+function addUser(){
+    player1_name = document.getElementById("player1_name").value;
+    player2_name = document.getElementById("player2_name").value;
+    
+    localStorage.setItem("player1", player1_name);
+    localStorage.setItem("player2", player2_name);
+    
+    window.location = "quiz_game_page.html";
+    }
